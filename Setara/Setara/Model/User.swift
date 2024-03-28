6857306fc931506d0dcf74218d4518cb8cf371b7
irvan <pradaindo24@gmail.com>
2024-03-28 15:25:51 +0700

    feat(Main): make architecture project

---
+++ Setara/Setara/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Setara
+//
+//  Created by Irvan P. Saragi on 28/03/24.
+//
+
+import Foundation
---
+++ Setara/Setara/ViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Setara
+//
+//  Created by Irvan P. Saragi on 28/03/24.
+//
+
+import Foundation
